8938c60159765b43ad3f48bf6c0360385b351f39
Andrew Daly <125076421+adaly6@users.noreply.github.com>
2023-11-09 21:33:58 -0600

    Update GameOfDice.py

--- GameOfDice.py
+++ GameOfDice.py
@@ -1,17 +1,3 @@
-"""
-Created on Thu Feb 16 18:50:13 2023
-
-Andrew Daly, Jacob Weisshaar
-
-The purpose of this code is for assignment 1 in Computational Thinking. 
-This is a simulation of a simple game of dice. Numbers that signify game over on the dice
-are 1, 2, and 3. When these numbers are rolled, the game ends. Numbers 4, 5, and 6
-signify the amount won by the player ($4, $5, or $6). The player may keep rolling until 
-game over numbers are rolled. Through 10,000 simulations of the game, we track the 
-average amount won, the probability that the game ends with a non-zero amount, and
-the largest amount won for a single game.
-
-"""
 
 #Import random module for random die rolls 1-6, import mean to find average amount won
 import random
@@ -51,17 +37,4 @@ avgWinnings = mean([totalWinnings / nonZeroGames])
 print("Probability of a non-zero winning: ", round(probNonZero, 2),"%")
 print("Average amount won: ", round(avgWinnings, 2))
 print("Largest amount won: ", round(largestWinning, 2))
-
-
-"""
-
-- Statistically, it is at least 50% likely to win $4 or more dollars
-when playing this game. 
-
-- Based on the output of 10,000 simulated die rolls, we would pay 
-$3 to play this game because the average amount won was greater than $3
-and there is a 50% to win $4 or more on the first roll and any roll thereafter.
-
-"""
-
 
